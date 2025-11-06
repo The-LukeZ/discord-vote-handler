f419f66ec3f48ed7d56d10e12e60fd80bfa41ab7
Luca Hentschel <luca.hentschel@2peaches.de>
2025-11-06 16:56:22 +0100

    fix: refactor CommandOptionResolver and enhance type definitions in index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,34 @@
+import type { Context } from "hono";
+import type { BlankInput } from "hono/types";
+import type { Collection } from "@discordjs/collection";
+import type {
+  APIAttachment,
+  APIInteractionDataResolvedChannel,
+  APIInteractionDataResolvedGuildMember,
+  APIRole,
+  APIUser,
+  Snowflake,
+} from "discord-api-types/v10";
+
 export * from "./db";
 export * from "./topgg";
 
-export {};
+export interface ResponseLike
+  extends Pick<Response, "arrayBuffer" | "bodyUsed" | "headers" | "json" | "ok" | "status" | "statusText" | "text"> {
+  body: Readable | ReadableStream | null;
+}
+
+export interface HonoEnv<Vars> {
+  Bindings: Env;
+  Variables: Vars;
+}
+
+export type MyContext<Vars> = Context<HonoEnv<Vars>, "/", BlankInput>;
+
+export interface APIInteractionDataResolvedCollections {
+  users?: Collection<Snowflake, APIUser>;
+  roles?: Collection<Snowflake, APIRole>;
+  members?: Collection<Snowflake, APIInteractionDataResolvedGuildMember>;
+  channels?: Collection<Snowflake, APIInteractionDataResolvedChannel>;
+  attachments?: Collection<Snowflake, APIAttachment>;
+}
