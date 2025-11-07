cc52accd9d99bd73ed6969e33a7124a19fc70dd4
Luca Hentschel <luca.hentschel@2peaches.de>
2025-11-07 23:06:44 +0100

    feat: add bot URL generation and test notice functionality for supported platforms

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,9 @@
+import { bold, heading } from "@discordjs/builders";
 import { SupportedPlatforms } from "../types";
+import { ApplicationIntegrationType } from "discord-api-types/v10";
+
+export const addBotUrl = (botId: string, integrationType: ApplicationIntegrationType = ApplicationIntegrationType.GuildInstall) =>
+  `https://discord.com/oauth2/authorize?integration_type=${integrationType}&client_id=${botId}` as const;
 
 /**
  * Generates the Platform webhook URL for a given application ID.
@@ -16,5 +21,23 @@ export const supportedPlatforms = {
 } satisfies Record<SupportedPlatforms, string>;
 
 export const GetSupportedPlatform = (platform: SupportedPlatforms) => supportedPlatforms[platform];
+
+/**
+ * List of platforms that support test vote payloads.
+ *
+ * Use this to determine whether to show the user the information that they should install the bot as a user app to receive test vote confirmation.
+ */
+export const platformsWithTests = ["topgg"];
+
+export const getTestNoticeForPlatform = (platform: SupportedPlatforms) => {
+  if (platformsWithTests.includes(platform)) {
+    return `${heading("Testing Votes", 3)}\nYou can use the **Send Test** feature on ${GetSupportedPlatform(
+      platform,
+    )} to verify that the webhook is working correctly. ${bold(
+      "[You need to install the bot as a user application to receive test votes.]()",
+    )}`;
+  }
+  return "";
+};
 
 export const hostnamePattern = /^[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)+$/i;
