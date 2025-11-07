1eece07ef18ad79691f8ace733190cb91e826f76
Luca Hentschel <luca.hentschel@2peaches.de>
2025-11-07 09:57:49 +0100

    feat: update generateSnowflake function to accept an optional timestamp parameter

--- src/snowflake.ts
+++ src/snowflake.ts
@@ -2,6 +2,6 @@ import { Snowflake } from "@sapphire/snowflake";
 
 const snowflake = new Snowflake(new Date("2025-11-04T00:00:00Z"));
 
-export function generateSnowflake(): bigint {
-  return snowflake.generate();
+export function generateSnowflake(timestamp?: Date): bigint {
+  return snowflake.generate({ timestamp });
 }
